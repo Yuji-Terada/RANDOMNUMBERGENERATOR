54ca3a034906faefa0ba3b784327632ea296f045
Yuji070902 <93475805+Yuji-Terada@users.noreply.github.com>
2023-05-04 00:34:42 +0900

    Add files via upload

--- theme.js
+++ theme.js
@@ -1,64 +1,206 @@
-var array = [];
+var maximum = 75;
+
+var arraynum = [];
 
 var a;
 var b;
 var temp;
 
 var allarraystr;
+var i;
+var i2;
 
 var showing = 0;
 
-for (let i = 0; i < 75; i++) {
-    array.push(i+1);
+const maxnum = document.getElementById("maxnum");
+
+for (i = 0; i < maximum; i++) {
+    i2 = i+1 < 10 ? "0" + String(i+1) : String(i+1);
+    arraynum.push(i2);
 }
+
+$('#title').click( function(){
+    if($("#setting").hasClass("panelactive") == false){
+        $("#setting").toggleClass("panelactive");
+        $("#blackbg").toggleClass("panelactive");
+    }
+});
+
+$('#setting>#close').click( function(){
+    $("#setting").removeClass("panelactive");
+    $("#blackbg").removeClass("panelactive");
+
+    maximum = maxnum.value;
+
+    arraynum = [];
+
+    for (i = 0; i < maximum; i++) {
+        i2 = i+1 < 10 ? "0" + String(i+1) : String(i+1);
+        arraynum.push(i2);
+    }
+
+    showing = 0;
+
+    for(i = 0; i < maximum*2; i++){
+        a = Math.floor( Math.random() * maximum );
+        b = Math.floor( Math.random() * maximum );
+    
+        temp = arraynum[a];
+
+        arraynum[a] = arraynum[b];
+        arraynum[b] = temp;
+    }
+
+    document.getElementById('num').textContent = arraynum[showing];
+    document.getElementById('showing').textContent = showing+1;
+    document.getElementById('num-2').textContent = arraynum[showing];
+    document.getElementById('showing-2').textContent = showing+1;
+    
+    allarraystr = ""
+
+    for(i = 0; i < maximum; i++){
+        i2 = i+1 < 10 ? "0" + String(i+1) : String(i+1);
+        allarraystr += String(i2) + "&emsp;" + String(arraynum[i]) + "<br>"
+    }
+
+    document.getElementById("allarray").innerHTML = allarraystr.replace(/\n/g, "<br>")
+    document.getElementById("allarray-2").innerHTML = allarraystr.replace(/\n/g, "<br>")
+
+});
+
+$('#setting>#back').click( function(){
+    $("#setting").removeClass("panelactive");
+    $("#blackbg").removeClass("panelactive");
+});
+
+
+
+
 
 $('#generate').click( function(){
     showing = 0;
 
-    for(let i = 0; i < 200; i++){
-        a = Math.floor( Math.random() * 75 );
-        b = Math.floor( Math.random() * 75 );
+    for(i = 0; i < maximum*2; i++){
+        a = Math.floor( Math.random() * maximum );
+        b = Math.floor( Math.random() * maximum );
     
-        temp = array[a];
+        temp = arraynum[a];
 
-        array[a] = array[b];
-        array[b] = temp;
+        arraynum[a] = arraynum[b];
+        arraynum[b] = temp;
     }
 
-    document.getElementById('num').textContent = array[showing];
+    document.getElementById('num').textContent = arraynum[showing];
     document.getElementById('showing').textContent = showing+1;
+    document.getElementById('num-2').textContent = arraynum[showing];
+    document.getElementById('showing-2').textContent = showing+1;
     
     allarraystr = ""
 
-    for(let i = 0; i < 75; i++){
-        allarraystr += String(i+1) + ":" + String(array[i]) + "<br>"
+    for(i = 0; i < maximum; i++){
+        i2 = i+1 < 10 ? "0" + String(i+1) : String(i+1);
+        allarraystr += String(i2) + "&emsp;" + String(arraynum[i]) + "<br>"
     }
 
     document.getElementById("allarray").innerHTML = allarraystr.replace(/\n/g, "<br>")
+    document.getElementById("allarray-2").innerHTML = allarraystr.replace(/\n/g, "<br>")
 });
 
 $('#prev').click( function(){
     showing = showing > 0 ? showing - 1 : showing;
 
-    document.getElementById('num').textContent = array[showing];
+    document.getElementById('num').textContent = arraynum[showing];
     document.getElementById('showing').textContent = showing+1;
+    document.getElementById('num-2').textContent = arraynum[showing];
+    document.getElementById('showing-2').textContent = showing+1;
 });
 
 $('#num').click( function(){
-    showing = showing < 74 ? showing + 1 : showing;
+    showing = showing < maximum-1 ? showing + 1 : showing;
 
-    document.getElementById('num').textContent = array[showing];
+    document.getElementById('num').textContent = arraynum[showing];
     document.getElementById('showing').textContent = showing+1;
+    document.getElementById('num-2').textContent = arraynum[showing];
+    document.getElementById('showing-2').textContent = showing+1;
 });
 
 $('#show').click( function(){
     $("#allarray").toggleClass("active");
+    $("#allarray-2").toggleClass("active");
 
     allarraystr = ""
 
-    for(let i = 0; i < 75; i++){
-        allarraystr += String(i+1) + ":" + String(array[i]) + "<br>"
+    for(i = 0; i < maximum; i++){
+        i2 = i+1 < 10 ? "0" + String(i+1) : String(i+1);
+        allarraystr += String(i2) + "&emsp;" + String(arraynum[i]) + "<br>"
     }
 
     document.getElementById("allarray").innerHTML = allarraystr.replace(/\n/g, "<br>")
+    document.getElementById("allarray-2").innerHTML = allarraystr.replace(/\n/g, "<br>")
+});
+
+
+
+
+
+$('#generate-2').click( function(){
+    showing = 0;
+
+    for(i = 0; i < maximum*2; i++){
+        a = Math.floor( Math.random() * maximum );
+        b = Math.floor( Math.random() * maximum );
+    
+        temp = arraynum[a];
+
+        arraynum[a] = arraynum[b];
+        arraynum[b] = temp;
+    }
+
+    document.getElementById('num').textContent = arraynum[showing];
+    document.getElementById('showing').textContent = showing+1;
+    document.getElementById('num-2').textContent = arraynum[showing];
+    document.getElementById('showing-2').textContent = showing+1;
+    
+    allarraystr = ""
+
+    for(i = 0; i < maximum; i++){
+        i2 = i+1 < 10 ? "0" + String(i+1) : String(i+1);
+        allarraystr += String(i2) + "&emsp;" + String(arraynum[i]) + "<br>"
+    }
+
+    document.getElementById("allarray").innerHTML = allarraystr.replace(/\n/g, "<br>")
+    document.getElementById("allarray-2").innerHTML = allarraystr.replace(/\n/g, "<br>")
+});
+
+$('#prev-2').click( function(){
+    showing = showing > 0 ? showing - 1 : showing;
+
+    document.getElementById('num').textContent = arraynum[showing];
+    document.getElementById('showing').textContent = showing+1;
+    document.getElementById('num-2').textContent = arraynum[showing];
+    document.getElementById('showing-2').textContent = showing+1;
+});
+
+$('#next-2').click( function(){
+    showing = showing < maximum-1 ? showing + 1 : showing;
+
+    document.getElementById('num').textContent = arraynum[showing];
+    document.getElementById('showing').textContent = showing+1;
+    document.getElementById('num-2').textContent = arraynum[showing];
+    document.getElementById('showing-2').textContent = showing+1;
+});
+
+$('#show-2').click( function(){
+    $("#allarray").toggleClass("active");
+    $("#allarray-2").toggleClass("active");
+
+    allarraystr = ""
+
+    for(i = 0; i < maximum; i++){
+        i2 = i+1 < 10 ? "0" + String(i+1) : String(i+1);
+        allarraystr += String(i2) + "&emsp;" + String(arraynum[i]) + "<br>"
+    }
+
+    document.getElementById("allarray").innerHTML = allarraystr.replace(/\n/g, "<br>")
+    document.getElementById("allarray-2").innerHTML = allarraystr.replace(/\n/g, "<br>")
 });
